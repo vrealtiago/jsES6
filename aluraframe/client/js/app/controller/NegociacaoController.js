e0abf29f493185a27d3caf41ee38783eedca3b20
Tiago <vrealtiago@gmail.com>
2018-03-19 20:57:46 +0100

    jsES6_02 - lesson 02

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -5,8 +5,21 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacoesView.update(model));
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver){
+                if(['adiciona', 'apagar'].includes(prop) && typeof(target[prop]) == typeof(Function)){
+                    return function(){
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+
+        });
+        /*this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));*/
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem = new Mensagem();
