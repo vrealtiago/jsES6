ad0a383cd914950185450bbc2211c261dfdadddb
Tiago <vrealtiago@gmail.com>
2018-03-21 01:34:01 +0100

    jsES6_02 - lesson 06

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -1,6 +1,7 @@
 class NegociacaoController{
 
     constructor(){
+        this._ordemAtual = '';
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
@@ -9,7 +10,7 @@ class NegociacaoController{
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia');
+            'adiciona', 'esvazia', 'ordena', 'inverteOrdem');
 
 	    this._mensagem = new Bind(
 		    new Mensagem(), new MensagemView($('#mensagemView')),
@@ -18,9 +19,13 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._cleanForm();
+        try{
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação adicionada com sucesso';
+            this._cleanForm();
+        }catch(erro){
+            this._mensagem.texto = erro;
+        }
     }
 
 	importaNegociacoes(){
@@ -52,6 +57,15 @@ class NegociacaoController{
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+
+    ordena(coluna){
+        if(this._ordemAtual == coluna){
+            this._listaNegociacoes.inverteOrdem();
+        }else{
+	        this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
+        }
+        this._ordemAtual = coluna;
     }
 
 }
--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -8,10 +8,10 @@ class DateHelper{
         return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
     static textoParaData(texto){
-        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
-            throw new Error('Date need to be yyyy-MM-dd');
+        if(!/\d{2}\/\d{2}\/\d{4}$/.test(texto))
+            throw new Error('Data deve estar no formato dd/mm/aaaa');
 
-       return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+       return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
     }
 
 }
